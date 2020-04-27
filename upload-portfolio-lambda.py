a1e8e3795116126f91132eccab4fdc7f516daffd
Jay Arun <jshreearunkumar@gmail.com>
2020-04-27 06:00:31 -0400

    Updated lamda file

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,45 +1,21 @@
 import boto3
+from botocore.client import Config
 import StringIO
 import zipfile
 import mimetypes
 
 
-def lambda_handler(event, context):
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    sns = boto3.resource('sns')
-    topic = sns.Topic('arn:aws:sns:us-east-1:026796352901:deployPortfolioTopic')
-    location = {
-        "bucketName": 'portfoliobuild.jblog.click',
-        "objectKey": 'portfoliobuild.zip'
-    }
-    try:
-        job = event.get("CodePipeline.job")
+portfolio_bucket = s3.Bucket('portfolio.jblog.click')
+build_bucket = s3.Bucket('portfoliobuild.zip')
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-        if job:
-            for artifacts in job["data"]["inputArtifacts"]:
-                if artifacts["name"] == "MyAppBuild":
-                    location = artifacts["location"]["s3Location"]
-        print "Building portfolio from " + str(location)
-        s3 = boto3.resource('s3')
-        portfolio_bucket = s3.Bucket('portfolio.jblog.click')
-        build_bucket = s3.Bucket(location["bucketName"])
-        portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
-
-        with zipfile.ZipFile(portfolio_zip) as myzip:
-            for nm in myzip.namelist():
-                obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj,nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        print "Job done"
-        topic.publish(Subject="Portfolio", Message="This is message to inform Portfolio got deployed successfully")
-        if job:
-            codepipeline = boto3.client('codepipeline')
-            codepipeline.put_job_success_result(jobId=job["id"])
-
-    except:
-        topic.publish(Subject="Portfolio failed", Message="This is message to inform Portfolio was not deployed")
-        raise
-
-    return 'Hello from Lambda'
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj,nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
